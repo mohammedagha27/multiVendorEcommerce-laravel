db99a08f0766beece71ec85355068085e194ff0d
Mohammed alagha <modyagha7@gmail.com>
2022-12-26 15:13:12 +0200

    add commented lines notes in Store Model

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Store extends Model
 {
     use HasFactory;
+
+    //? if you don't follow on of the laravel standards yuo have to override the Model Class
+
+    // const CREATED_AT = 'created_at or any thing you want';
+    // const UPDATED_AT = 'updated_at';
+
+    // protected $connection = 'mysql';
+
+    // protected $table = 'stores';
+
+    // protected $primaryKey = 'id';
+
+    // protected $keyType = 'int';
+
+    // public $incrementing = true;
+
+    // public $timestamps = true;
 }
